f59c6e026b5375d40ffffd50f3fb0191233df399
gleb-britecore <gsev@diceus.com>
2018-03-09 22:28:58 +0200

    added configs and scripts for dev.* domain

---
+++ rebuild_prod.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+git pull
+
+docker build -t ba_carrier_portal:latest .
+
+
+docker run -v ~/public_html/prod:/var/dist/dist \
+        --env API_ROOT="'http://admin.briteapps.com'"  \
+        ba_carrier_portal:latest
+
+docker rm --force ba_carrier_portal__nginx
+
+docker run \
+    --name ba_carrier_portal__nginx \
+    -v ~/public_html:/usr/share/nginx/html:ro \
+    -v $PWD/deploy/default.conf:/etc/nginx/conf.d/default.conf \
+    -p 80:80 \
+    -d \
+    nginx
