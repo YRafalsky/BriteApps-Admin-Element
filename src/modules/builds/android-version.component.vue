86b175badb20ab415f92e364858992636215760f
YRafalsky <ministrybadblood@gmail.com>
2018-03-28 15:26:51 +0300

    start using new tracks state EP

--- src/modules/builds/android-version.component.vue
+++ src/modules/builds/android-version.component.vue
@@ -1,4 +1,5 @@
 <template>
+    <div>
   <div>
     Android promotions
     <el-tag
@@ -14,6 +15,22 @@
       type="primary"
       close-transition>BETA: {{betaBuild | capitalize}}</el-tag>
   </div>
+  <div>
+    Apple promotions
+    <el-tag
+      v-loading="loading"
+      style="min-width: 100px;"
+      class="u-text--center"
+      type="success"
+      close-transition>PROD: {{appleProdBuild | capitalize}}</el-tag>
+    <el-tag
+      v-loading="loading"
+      style="min-width: 100px;"
+      class="u-text--center"
+      type="primary"
+      close-transition>BETA: {{appleBetaBuild | capitalize}}</el-tag>
+  </div>
+    </div>
 </template>
 
 
@@ -34,17 +51,23 @@ export default {
   },
   computed: {
     prodBuild () {
-      return this.builds && this.builds.production? this.builds.production: 'None'
+      return this.builds && this.builds['Play Market'].production? this.builds['Play Market'].production: 'None'
     },
     betaBuild () {
-      return this.builds && this.builds.beta? this.builds.beta: 'None'
+      return this.builds && this.builds['Play Market'].beta? this.builds['Play Market'].beta: 'None'
+    },
+    appleProdBuild () {
+      return this.builds && this.builds['App Store'].production? this.builds.production['App Store']: 'None'
+    },
+    appleBetaBuild () {
+      return this.builds && this.builds['App Store'].beta? this.builds['App Store'].beta: 'None'
     }
   },
   methods: {
     loadAndroidPromotesState () {
       this.loading = true
       clearTimeout(this.loadAndroidTimerRef)
-      axios.get(`${config.url}/company/${this.companyId}/promoted/android`)
+      axios.get(`${config.url}/company/${this.companyId}/promoted/tracks`)
         .then(response => {
           this.builds = response.data
           this.dataValid = true
