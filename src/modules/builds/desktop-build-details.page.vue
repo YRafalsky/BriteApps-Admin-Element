75c4fe5230a6780200374f65744fce582823dafc
gleb-britecore <gsev@diceus.com>
2018-04-04 19:13:29 +0300

    Small improvements to desktop builds view

--- src/modules/builds/desktop-build-details.page.vue
+++ src/modules/builds/desktop-build-details.page.vue
@@ -9,7 +9,7 @@
                 <el-button icon="el-icon-refresh" @click="invalidateBuildStatus()">Invalidate Status</el-button>
             </div>
             <div class="u-text--center u-mt3">
-                <ba-status-label v-loading="loadingStatus" element-loading-background="rgba(0, 0, 0, 0.1)" v-if="build"
+                <ba-status-label element-loading-background="rgba(0, 0, 0, 0.1)" v-if="build"
                                  :build="build"></ba-status-label>
             </div>
 
@@ -36,7 +36,7 @@
 
             </el-collapse-transition>
             <div class="row" v-if="!loading && build">
-                <div class="col-12 u-text--center">
+                <div class="col-12 u-text--center" v-if="!loading && build && build.status=='SUCCEEDED'">
                     <el-button @click="dialogVisible = true" type="success"
                                element-loading-background="rgba(0, 0, 0, 0.1)">
                         <icon name="rocket"></icon>
@@ -56,10 +56,9 @@
                                 </div>
                             </el-tag>
 
-                            <div class="u-mb2"><span class="u-text--sm">Target Bucket: </span> <a target="_blank"
-                                                                                                  referrerpolicy="no-referrer"
-                                                                                                  :href="'http://' + build.actions.publish.to_bucket + '.s3-website-us-east-1.amazonaws.com'"><span
-                                    class="u-text--success c-heading--bold">  {{build.actions.publish.to_bucket}}</span></a>
+                            <div class="u-mb2"><span class="u-text--sm">Target Bucket: </span>
+                                <a target="_blank" referrerpolicy="no-referrer" :href="liveVersionHref">
+                                    <span class="u-text--success c-heading--bold">  {{build.actions.publish.to_bucket}}</span></a>
                             </div>
 
 
@@ -72,8 +71,9 @@
                 </div>
                 <div class="col-12">
                     <h3 class="u-text--center u-mt4">Build Preview</h3>
-                    <a :href="buildPreviewLink" target="_blank"
+                    <a :href="buildPreviewLink" target="_blank" v-if="!loading && build && build.status=='SUCCEEDED'"
                        class="desktop__preview-link u-m4 c-heading__section u-text--center">Open In Separate Tab</a>
+                    <div class="u-m4 u-text--center u-text--light" v-else>Preview will be available upon build completion</div>
                 </div>
             </div>
         </div>
@@ -82,92 +82,108 @@
 </template>
 
 <script>
-import axios from 'axios'
-import config from '@/config'
-import ElTag from 'element-ui/packages/tag/src/tag'
-export default {
-  components: {
-    ElTag,
-  },
-  data () {
-    let buildId = this.$route.params.buildId
-    let companyId = this.$route.params.companyId
-    return {
-      dialogVisible: false,
-      loading: true,
-      loadingStatus: false,
-      buildId,
-      showBuildTechnicalDetails: false,
-      build: {},
-      loadBuildTimerRef: null,
-      companyId,
-    }
-  },
-  computed: {
-    buildPreviewLink () {
-      return this.build.aws_build_preview
+  import axios from 'axios'
+  import config from '@/config'
+  import ElTag from 'element-ui/packages/tag/src/tag'
+
+  export default {
+    components: {
+      ElTag,
     },
-    humanBuildId () {
-      return this.build ? this.build.version + '.' + this.build.build_number : 'UNDEFINED'
-    }
-  },
-  methods: {
-    invalidateBuildStatus () {
-      this.loading = true
-      axios.patch(config.desktop_builds_details + this.buildId + '/')
+    data () {
+      let buildId = this.$route.params.buildId
+      let companyId = this.$route.params.companyId
+      return {
+        dialogVisible: false,
+        loading: true,
+        buildId,
+        showBuildTechnicalDetails: false,
+        build: {},
+        loadBuildTimerRef: null,
+        companyId,
+      }
+    },
+    computed: {
+      liveVersionHref () {
+        return 'http://' + this.build.actions.publish.to_bucket + '.s3-website-us-east-1.amazonaws.com'
+      },
+      buildPreviewLink () {
+        return this.build.aws_build_preview
+      },
+      humanBuildId () {
+        return this.build ? this.build.version + '.' + this.build.build_number : 'UNDEFINED'
+      }
+    },
+    methods: {
+      invalidateBuildStatus () {
+        this.loading = true
+        axios.patch(config.desktop_builds_details + this.buildId + '/')
         .then(response => {
           this.loading = false
           this.build = response.data
         })
-    },
-    publishBuild () {
-      axios.post(config.desktop_builds_details + this.buildId + '/publish/', {})
+      },
+      publishBuild () {
+        this.dialogVisible = false
+        this.$notify({
+          message: 'Site publishing queued...',
+          type: 'info'
+        })
+        axios.post(config.desktop_builds_details + this.buildId + '/publish/', {})
         .then(_ => {
           console.log('_', _.data)
-        }
-        )
+          this.$notify({
+            message: 'Site for ' + this.build.company +'successfully published.',
+            type: 'success'
+          })
+        })
+        .catch(_ => {
+          this.$notify({
+            title: 'Error while publishing site live',
+            message: _.data,
+            type: 'error',
+            duration: 0
+          })
+        })
         .finally(() => {
           this.loading = false
         })
-    },
-    loadBuild () {
-      if (this.build.status && ['queued', 'IN_PROGRESS'].indexOf(this.build.status) === -1) {
-        this.loading = false
-        return
-      }
-      if (!this.loading) {
-        this.loadingStatus = true
-      }
-      axios.get(config.desktop_builds_details + this.buildId + '/')
+      },
+      loadBuild () {
+        if (this.build.status && ['queued', 'IN_PROGRESS'].indexOf(this.build.status) === -1) {
+          this.loading = false
+          return
+        }
+        axios.get(config.desktop_builds_details + this.buildId + '/')
         .then(response => {
           if (!this.loading) {
-            this.loadingStatus = false
+            this.loading = false
           }
           this.loading = false
           this.build = response.data
           if (['queued', 'IN_PROGRESS'].indexOf(this.build.status) !== -1) {
-            this.loadingStatus = true
+            this.loading = true
             this.initBuildStatus = this.build.status
             this.loadBuildTimerRef = setTimeout(this.loadBuild, 15000)
           } else {
-            this.loadingStatus = false
+            this.loading = false
           }
         })
         .finally(() => {
           this.loading = false
         })
+      },
     },
-  },
-  beforeMount () {
-    this.loading = true
-    this.initBuildStatus = null
-    this.loadBuild()
-  },
-  beforeDestroy () {
-    clearTimeout(this.loadBuildTimerRef)
-    clearTimeout(this.loadApplePromoteTimerRef)
+    beforeMount () {
+      this.loading = true
+      this.initBuildStatus = null
+      this.loadBuild()
+    },
+    beforeDestroy () {
+      clearTimeout(this.loadBuildTimerRef)
+      clearTimeout(this.loadApplePromoteTimerRef)
+    }
   }
-}
 </script>
 <style>
     .build-details__root .el-loading-mask {
