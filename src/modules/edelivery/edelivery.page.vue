19fa45c11cde6b6904549f325456dfd71209d4cd
gleb-britecore <gsev@diceus.com>
2018-05-02 17:48:03 +0300

    UI for edelivery page, use en locale

--- src/modules/edelivery/edelivery.page.vue
+++ src/modules/edelivery/edelivery.page.vue
@@ -1,27 +1,28 @@
 <template>
   <div class="edelivery">
     <ba-header activeModule="E-Delivery"></ba-header>
-    <div class="edelivery__content">
-      <h2 class="c-heading__page">E-Delivery dashboard</h2>
+    <div class="edelivery__content u-p4">
+      <h2 class="c-heading__page u-mt4 u-pt2">E-Delivery dashboard</h2>
       <div class="edelivery__table">
-
-        <div>
+        <div class="edelivery__filters u-mt4">
           <el-form inline>
-            <el-form-item label="From"><el-date-picker></el-date-picker></el-form-item>
-            <el-form-item label="To"><el-date-picker></el-date-picker></el-form-item>
-            <el-form-item><el-button @click="loadPage(1)">Filter</el-button></el-form-item>
+
+
+            <el-form-item label="From" class="line-height-3"><el-date-picker v-model="range.from"></el-date-picker></el-form-item>
+            <el-form-item label="To" class="line-height-3"><el-date-picker  v-model="range.to"></el-date-picker></el-form-item>
+
+            <el-form-item><el-button @click="loadPage(1)" icon="el-icon-search">Filter</el-button></el-form-item>
           </el-form>
         </div>
 
         <div class="u-mt3">
-          <el-button-group>
-            <el-button>Check all</el-button>
-            <el-button>Check none</el-button>
-          </el-button-group>
-          <el-button @click="doSomething">DO SOMETHING</el-button>
-          <el-button>SEND</el-button>
-          <el-button>Mark as sent</el-button>
-          <el-table class="u-mt3" :data="items" style="width: 100%">
+          <!--<el-button-group>-->
+            <!--<el-button @click="checkAll()">Check all</el-button>-->
+            <!--<el-button @click="uncheckAll()">Check none</el-button>-->
+          <!--</el-button-group>-->
+          <el-button @click="willSend()" :disabled="!areButtonsEnabled" icon="el-icon-message">SEND</el-button>
+          <el-button @click="willMarkAsSent()" :disabled="!areButtonsEnabled" icon="el-icon-check">Mark as sent</el-button>
+          <el-table  @selection-change="selectionDidChange" v-loading="loading" ref="edeliveryTable" class="u-mt3" :data="items" style="width: 100%">
             <el-table-column type="selection" width="55"></el-table-column>
             <el-table-column prop="policy_number" label="Policy Number" width="180"></el-table-column>
             <el-table-column prop="title" label="Title" width="180"></el-table-column>
@@ -49,6 +50,7 @@ import ElButton from 'element-ui/packages/button/src/button'
 import axios from 'axios'
 import config from '@/config'
 import qs from 'qs'
+import _ from 'lodash'
 
 export default {
   components: {
@@ -57,52 +59,129 @@ export default {
   },
   name: 'ba-edelivery',
   data () {
+    let now = new Date()
+    let from = new Date()
+    from.setFullYear(now.getFullYear() - 1);
     return {
       companyId: this.$route.params.companyId,
+      range: {
+        from: from,
+        to: now,
+      },
       items: [],
+      loading: true,
       total: 0,
       perPage: 50,
-      fromDate: '2017-10-10',
-      toDate: '2018-02-20',
-      ignoreState: false,
+      selectedItems: [],
+      ignoreState: true,
       onlyElectronicDelivery: true
     }
+  },
+  computed: {
+    areButtonsEnabled () {
+      return !this.loading && this.selectedItems.length > 0
+    }
+  },
+  beforeMount () {
+    this.loading = true
+    this.loadInitial()
   },
   methods: {
     loadInitial () {
       this.loadPage(1)
     },
+    setSelectionStateForAll (desiredCheckedState) {
+      if (this.items) {
+        this.items.forEach(row => {
+          this.$refs.edeliveryTable.toggleRowSelection(row, desiredCheckedState)
+        })
+      }
+    },
+    checkAll () {
+      this.setSelectionStateForAll(true)
+    },
+    uncheckAll () {
+      this.setSelectionStateForAll(false)
+    },
     loadPage (page) {
       console.log('loadPage')
-      axios.post(`${config.url}/company/${this.companyId}/get_edelivery/`, {page: page}).then((ret) => {
-        debugger
-        this.items = ret.data.data.documents
-        this.total = ret.data.data.total
-      })
-    },
-    doSomething () {
-      var page = 1
-      axios.post(`${config.url}/company/${this.companyId}/get_edelivery/`,{
-        from_date: this.fromDate,
-        to_date: this.toDate,
+      this.loading = true
+      axios.post(`${config.url}/company/${this.companyId}/get_edelivery/`, {
+        from_date: this.range.from,
+        to_date: this.range.to,
         per_page: this.perPage,
         page: page,
+
         ignore_state: this.ignoreState,
-        only_electronic_delivery: this.onlyElectronicDelivery
+        // todo uncomment
+        only_electronic_delivery: this.onlyElectronicDelivery,
       }).then((ret) => {
-        console.log(ret.data)
+        // debugger
+        this.items = ret.data.data.documents
+        this.total = ret.data.data.total
+        this.perPage = ret.data.data.page_size
       })
+      .finally(() => {
+        this.loading = false
+      })
+    },
+    selectionDidChange (selectedItems) {
+      this.selectedItems = selectedItems
+    },
+    sendNow (selectedItems) {
+      let fileIds = _.map(selectedItems, function (item) {
+        return item['file_id']
+      })
+      axios.post(`${config.url}/company/${this.companyId}/make_edelivery/`, {file_ids: fileIds})
+      .then(response => {
+        this.$message({
+          type: 'success',
+          message: 'EMails sent.'
+        })
+      })
+      .catch(response => {
+        this.$message({
+          type: 'error',
+          message: 'Failed to send emails.'
+        });
+      })
+      .finally(() => this.loadInitial())
+
+    },
+    willSend () {
+      let count = this.selectedItems.length
+      this.$confirm(`This will send ${count} email(s). Continue?`, 'Emails are about to be sent', {
+        confirmButtonText: 'Send',
+        cancelButtonText: 'Cancel',
+        type: 'info'
+      }).then(() => {
+        this.sendNow(this.selectedItems)
+
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: 'Operation canceled'
+        });
+      });
     }
   },
 }
 </script>
 
 <style scoped>
-  .edelivery__content {
-    padding-top: 3em;
-  }
-
-  .edelivery__table {
-    padding: 3em;
+  .line-height-3 {
+    line-height: 2em;
   }
 </style>
+
+<style>
+  .edelivery .el-pager li.active {
+    color: #fff;
+    background-color: #409EFF;
+  }
+  .edelivery .el-loading-mask{
+    z-index: 799;
+  }
+
+</style>
+
