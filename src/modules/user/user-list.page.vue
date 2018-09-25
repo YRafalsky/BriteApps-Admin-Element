d1db1f95cf9eac102eeaff977e90929fa9821afa
vlad-kovalenko <vkoval@diceus.com>
2018-09-25 14:36:05 +0300

    func optimization added

--- src/modules/user/user-list.page.vue
+++ src/modules/user/user-list.page.vue
@@ -165,7 +165,7 @@ export default {
     },
     closeModal () {
       this.centerDialogVisible = false
-      this.inputData = ''
+      this.reset()
     },
     reset () {
       this.inputData = ''
