ab41c2f116c03b1650c2832b209572f7384e2eec
vlad-kovalenko <vkoval@diceus.com>
2018-10-10 11:31:40 +0300

    function saveModifiedHtmlContent fixed in single template

--- src/modules/templates/single-template.component.vue
+++ src/modules/templates/single-template.component.vue
@@ -92,10 +92,13 @@ export default {
         })
     },
     saveModifiedHtmlContent () {
+      let companyId = this.$route.params.companyId
+
       let payload = {
         template: this.template,
         text_content: this.textValue,
         html_content: this.htmlValue,
+        companyId,
       }
       this.isEditingHtml = false
       this.saveSingleTemplate(payload)
@@ -104,6 +107,8 @@ export default {
             message: 'Content saved successfully',
             type: 'success'
           })
+        }, (e) => {
+          console.log('error', e)
         })
     }
   }
